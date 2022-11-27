6625e964d77a3fde2c020e2120c16a4a425bf306
War3Map <barcelonasuper@mail.ru>
2022-11-27 19:12:22 +1000

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //print("Текст");
+        //Destroy(this.gameObject);
+        //var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        //cube.GetComponent<Transform>().position = new Vector3(7, (float)0.74, 24);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(this.gameObject);
+
+    }
+}
